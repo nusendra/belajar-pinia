78fc03f82edc3fe1db93477909aaa7813c3aba95
Nusendra Hanggarawan <admin@nusendra.com>
2022-02-06 03:57:34 +0700

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  ini footer
+  <br />
+  <button @click="increment">increment +</button>
+  <button @click="decrement">decrement -</button>
+  <button @click="reset">reset</button>
+  <br />
+  <button @click="incrementWithAction">increment with action</button>
+  <button @click="decrementWithAction">decrement with action</button>
+  <button @click="incrementByWithAction(5)">increment with action by 5</button>
+  <hr />
+  <button @click="fetchPokemon('ditto')">fetch ditto</button>
+  <button @click="resetPokemon">resetPokemon</button>
+</template>
+
+<script setup>
+import { useCounterStore } from "../stores/counter";
+import { usePokemonStore } from "../stores/pokemon";
+
+const counter = useCounterStore();
+const pokemon = usePokemonStore();
+
+const increment = () => {
+  counter.$patch((state) => {
+    state.count++;
+  });
+};
+const decrement = () => {
+  counter.$patch((state) => {
+    state.count--;
+  });
+};
+const reset = () => {
+  counter.$reset();
+};
+
+const incrementWithAction = () => {
+  counter.increment();
+};
+const decrementWithAction = () => {
+  counter.decrement();
+};
+const incrementByWithAction = (amount) => {
+  counter.incrementBy(amount);
+};
+
+const fetchPokemon = (name) => {
+  pokemon.getPokemonDetails(name);
+};
+const resetPokemon = () => {
+  pokemon.$reset();
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  ini header
+  <pre>state : {{ counter }}</pre>
+  <pre>getters : {{ getCount }}</pre>
+  <pre>getters : {{ doubleCount }}</pre>
+  <pre>getters : {{ dividedByTwo }}</pre>
+  <pre>pokemon : {{ pokemon }}</pre>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useCounterStore } from "../stores/counter";
+import { usePokemonStore } from "../stores/pokemon";
+
+const counterStore = useCounterStore();
+const pokemonStore = usePokemonStore();
+
+const counter = computed(() => counterStore.count);
+const getCount = computed(() => counterStore.getCount);
+const doubleCount = computed(() => counterStore.doubleCount);
+const dividedByTwo = computed(() => counterStore.dividedBy(2));
+
+const pokemon = computed(() => pokemonStore.pokemonDetails);
+
+counterStore.$subscribe((mutation, state) => {
+  console.log(mutation, state);
+});
+</script>
